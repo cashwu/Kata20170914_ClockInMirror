41d27385070044edb38b82c83c1fb7f44bd0b6dc
cash <cash@cashwu.com>
2017-09-14 20:58:34 +0800

    input 0100, 0300, 0900 test case

--- Kata20170914_ClockInMirror/ClockInMirrorTests.cs
+++ Kata20170914_ClockInMirror/ClockInMirrorTests.cs
@@ -12,6 +12,24 @@ namespace Kata20170914_ClockInMirror
             WhatIsTheTimeInMirrorShouldBe("12:00", "12:00");
         }
 
+        [TestMethod]
+        public void input_0100_should_return_1100()
+        {
+            WhatIsTheTimeInMirrorShouldBe("11:00", "01:00");
+        }
+
+        [TestMethod]
+        public void input_0300_should_return_0900()
+        {
+            WhatIsTheTimeInMirrorShouldBe("09:00", "03:00");
+        }
+
+        [TestMethod]
+        public void input_0900_should_return_0300()
+        {
+            WhatIsTheTimeInMirrorShouldBe("03:00", "09:00");
+        }
+
         private static void WhatIsTheTimeInMirrorShouldBe(string expected, string timeInMirror)
         {
             var kata = new Kata();
@@ -24,7 +42,12 @@ namespace Kata20170914_ClockInMirror
     {
         public string WhatIsTheTime(string timeInMirror)
         {
-            return timeInMirror;
+            var hour = timeInMirror.Split(':')[0];
+            var min = timeInMirror.Split(':')[1];
+
+            hour = hour == "12" ? "12": (12 - int.Parse(hour)).ToString("0#");
+
+            return $"{hour}:{min}";
         }
     }
 }
