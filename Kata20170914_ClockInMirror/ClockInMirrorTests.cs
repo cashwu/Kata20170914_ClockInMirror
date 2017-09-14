c5ee7563b96fb4b83a0322593c153ffb42893b03
cash <cash@cashwu.com>
2017-09-14 20:40:11 +0800

    input 1200 test case

---
+++ Kata20170914_ClockInMirror/ClockInMirrorTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kata20170914_ClockInMirror
+{
+    [TestClass]
+    public class ClockInMirrorTests
+    {
+        [TestMethod]
+        public void input_1200_should_return_1200()
+        {
+            WhatIsTheTimeInMirrorShouldBe("12:00", "12:00");
+        }
+
+        private static void WhatIsTheTimeInMirrorShouldBe(string expected, string timeInMirror)
+        {
+            var kata = new Kata();
+            var actual = kata.WhatIsTheTime(timeInMirror);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class Kata
+    {
+        public string WhatIsTheTime(string timeInMirror)
+        {
+            return timeInMirror;
+        }
+    }
+}
